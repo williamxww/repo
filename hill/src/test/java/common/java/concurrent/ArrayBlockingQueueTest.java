b0fb6db1781e04031fc574932d7eedbde5e62c13
vivi <vivid_xiang@163.com>
2016-07-17 12:25:57 +0800

    ArrayBlockingQueue

--- hill/src/test/java/common/java/concurrent/ArrayBlockingQueueTest.java
+++ hill/src/test/java/common/java/concurrent/ArrayBlockingQueueTest.java
@@ -13,6 +13,13 @@ import java.util.concurrent.TimeUnit;
  */
 public class ArrayBlockingQueueTest {
 
+	// 线程数小于5时直接新增线程处理任务,当线程数达到5后则用SynchronousQueue存储任务,
+	// 当queue满了则又新增线程来处理任务,当线程数达到最高值100则采用丢弃策略
+	// SynchronousQueue 插入取出插入取出...必须交替进行（放一个就满了）
+	// LinkedBlockingQueue无界队列任务会一直往里面添加知道撑爆，最大线程数此时相当于无效
+	private static ExecutorService threadPool = new ThreadPoolExecutor(5, 100, 20L, TimeUnit.SECONDS,
+			new ArrayBlockingQueue<Runnable>(10));
+
 	private BlockingQueue<String> blockingQueue = new ArrayBlockingQueue<String>(10);
 	
 	private void startReading(){
@@ -36,20 +43,43 @@ public class ArrayBlockingQueueTest {
 	}
 	
 	private void startStoring() {
-		// 线程数小于5时直接新增线程处理任务,当线程数达到5后则用SynchronousQueue存储任务,
-		// 当queue满了则又新增线程来处理任务,当线程数达到最高值100则采用丢弃策略
-		// SynchronousQueue 插入取出插入取出...必须交替进行（放一个就满了）
-		// LinkedBlockingQueue无界队列任务会一直往里面添加知道撑爆，最大线程数此时相当于无效
-		ExecutorService threadPool = new ThreadPoolExecutor(5, 100,
-				20L, TimeUnit.SECONDS,
-				new ArrayBlockingQueue<Runnable>(10));
-		Runnable store = new Runnable() {
 
+		Thread t = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						String a = blockingQueue.poll(3, TimeUnit.SECONDS);
+						if (a == null) {
+							break;// 取不到则退出
+						}
+						handleStore(a);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				System.out.println("FINISHED");
+			}
+
+		}, "get-task-thread");
+		t.start();
+	}
+
+	private void handleStore(String v) {
+
+		Runnable store = new Runnable() {
 			@Override
 			public void run() {
 				
 			}
 			
 		};
+		threadPool.execute(store);
+	}
+
+	public static void main(String[] args) {
+		ArrayBlockingQueueTest demo = new ArrayBlockingQueueTest();
+		demo.startReading();
+		demo.startStoring();
 	}
 }
